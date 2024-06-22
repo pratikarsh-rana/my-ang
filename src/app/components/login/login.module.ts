a9b36f0151ff41bcb4b1fcbff394e0446646dc69
pratikarsh-rana31 <pratikarshrana@gmail.com>
2024-06-23 01:33:17 +0530

    login component update

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
